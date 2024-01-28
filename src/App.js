001a97d28c3ecc765ec8b14ed7565f639a663ffb
Kassem Santos <Kassemsantos28@gmail.com>
2024-01-28 01:14:01 -0300

    Mudanças gerais

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from '../src/Header/Header';
+import Sidebar from '../src/Sidebar/Sidebar';
+import Footer from '../src/Footer/Footer';
+import Main from '../src/Main/Main';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Sidebar />
+    <Header />
+    <Main/>
+    <Footer/>
     </div>
   );
 }
